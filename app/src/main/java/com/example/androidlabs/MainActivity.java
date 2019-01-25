6f2ef697ad48e1cde0c6fb73b14b30f50cf22ff9
yangbushi <gyang@inteloom.com>
2019-01-24 21:14:38 -0500

    Initial commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.androidlabs;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.widget.TextView.BufferType.NORMAL;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PROFILEINTENT = "ProfileActivity";
+    private EditText emailEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.lab3);
 
+        emailEdit = (EditText) findViewById(R.id.emailAddress);
+        SharedPreferences prefs = getSharedPreferences("shared.txt", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("emailAddress", "");
+        emailEdit.setText(emailAddress.toCharArray(), 0, emailAddress.length());
+
+        Button mButton = (Button) findViewById(R.id.lab3LoginButton);
+        mButton.setOnClickListener(v->{
+            Intent startProfileIntent = new Intent(this, ProfileActivity.class);
+            startProfileIntent.putExtra("emailAddress", emailEdit.getText().toString());
+            startActivity(startProfileIntent);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs = getSharedPreferences("shared.txt", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("emailAddress", emailEdit.getText().toString());
+        edit.commit();
     }
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,87 @@
+package com.example.androidlabs;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageButton mImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e(ACTIVITY_NAME, "In function: onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Intent loginIntent = getIntent();
+        EditText emailEdit = (EditText) findViewById(R.id.lab3EmailEdit);
+        emailEdit.setText(loginIntent.getStringExtra("emailAddress"));
+
+        mImageButton = findViewById(R.id.imageButton);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(ACTIVITY_NAME, "In function: onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e(ACTIVITY_NAME, "In function: onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e(ACTIVITY_NAME, "In function: onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.e(ACTIVITY_NAME, "In function: onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(ACTIVITY_NAME, "In function: onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(ACTIVITY_NAME, "In function: onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+}
